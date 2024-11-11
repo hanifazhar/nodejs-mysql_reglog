e6f9c821d3a74312f10c396889e0cd42f4e44773
hanifazhar <azharhanif04@gmail.com>
2024-11-11 22:13:44 +0700

    add some code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 const express = require('express');
 const app = express();
+
+app.use('views engine', 'ejs');
+
+//ROUTE
+app.get('/', (req, res) => {
+    res.send("Halaman Home")
+})
+app.get('/login', (req, res) => {
+    res.send("Halaman Login")
+})
+app.post('/login', (req, res) => {
+    res.send("User Login")
+})
+app.get('/register', (req, res) => {
+    res.send("Halaman Register")
+})
+app.post('/register', (req, res) => {
+    res.send("User Register")
+})
+app.get('/logout', (req, res) => {
+    res.send("Logout User")
+})
+app.get('/admin', (req, res) => {
+    res.send("Halaman bagi yang sudah login")
+})
+
+app.listen(3000, () => {
+    console.log("Server berjalan di server : 3000")
+})
