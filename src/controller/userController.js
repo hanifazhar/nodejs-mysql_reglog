4b95d57288a0ee3e015110386091a198615f2429
hanifazhar <azharhanif04@gmail.com>
2024-11-12 11:42:15 +0700

    add register logic code

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,7 +3,18 @@ const bcrypt = require('bcrypt');
 
 const userController = {
     register: async(req, res) => {
-        
+        const fullname = req.body.fullname;
+        const username = req.body.username;
+        const password = await bcrypt.hash(req.body.password, 10);
+
+        connection.query(
+            `INSERT INTO users (fullname, username, password) VALUES('${fullName}','${username}','${password}' )`, (err, result) => {
+                if(err) throw err
+
+                console.log("Data Berhasil Ditambahkan");
+                res.redirect('/login')
+            }
+        )
     },
     login: (req, res) => {
         
--- src/database/database.js
+++ src/database/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-    host:localhost,
-    user:root,
+    host:'localhost',
+    user:'root',
     password:'',
-    database:login-register
+    database:'login-register'
 })
 
 module.exports = { connection }
